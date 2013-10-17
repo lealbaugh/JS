13214f67da046221d1af8fc15376bfdb6e98fada
Lea Albaugh <l.e.albaugh@gmail.com>
2013-10-17 19:57:55 -0400

    Supports arbitrary numbers of stacks, and fixes insidious dish color bug.

--- marys_dishes.js
+++ marys_dishes.js
@@ -1,10 +1,5 @@
 //Game settings, globals
 
-var dishheight = 7;
-var dishwidth = 50;
-var dishvertspacing = 3;
-var dishhorspacing = 10;
-
 var canvas;
 var ctx;
 
@@ -15,16 +10,23 @@ var mouseY = 0;
 var carrying = false;
 var carried;
 
+var numberofstacks = 3;
+var dishheight = 7;
+var dishwidth = 50;
+var dishvertspacing = 3;
+var dishhorspacing = 10;
+
+
 
 //Object definitions
-
 
 var StackView = function(stack, ctx) {
    var drawOps = [];
    stack.bind("push", function(x) {
        drawOps.push(function() { 
         ctx.fillStyle= x.color;
-        ctx.fillRect(x.x, x.y, dishwidth, dishheight); });
+        ctx.fillRect(x.x, x.y, dishwidth, dishheight); 
+      });
    }); //we're pushing a function onto Stack's bindings array which will push "drawing a rectangle" onto StackView's drawOps array 
 
     stack.bind("pop", function(x) {
@@ -47,14 +49,21 @@ var Stack = function(xposition, ctx) {
      item.x= this.x || 20;
      item.y= canvas.height-((dishheight+dishvertspacing)*(array.length+1));
      array.push(item);
+
+        for (var stack in stacks){
+          for (var i=0; i<stacks[stack].length(); i++){
+            console.log(stacks[stack].all()[i].color);
+          }     
+        }
+
      bindings["push"].forEach(function(x) {
          x(item);
      });
   };
   this.pop = function() {
-    // bindings["pop"].forEach(function(x) {
-    //     x();
-    // });
+      bindings["pop"].forEach(function(x) { //apparently this is not really necessary? unclear
+         x();
+     });
      return array.pop();
   };
   this.bind = function(operation, callback) {
@@ -63,6 +72,9 @@ var Stack = function(xposition, ctx) {
   };
   this.length = function(){
       return array.length;
+  }
+  this.all = function(){
+    return array;
   }
 
   var stackview = new StackView(this, ctx);
@@ -75,10 +87,13 @@ Stack.prototype = {   //we don't actually need to put anything in the prototype 
 };
 
 
+
+
 var Dish = function(number, color, x, y){
   this.x = x;
   this.y = y;
-  this.color = '#'+Math.floor(Math.random()*16777215).toString(16);
+  this.color = (Math.random().toString(16) + '000000').slice(2, 8);
+  //['red','blue','green','cyan','orange','black','purple','yellow'][Math.floor(Math.random()*8)]
   this.number = number;
 
 }
@@ -86,10 +101,15 @@ var Dish = function(number, color, x, y){
 
 
 
-//------------------------------------------------------------------------
-
+//Game functions
 
 function main(){
+  initialize();
+  gameLoop();   //yeah, oh hey, that
+}
+
+
+function initialize(){
   canvas = document.getElementById('visible-canvas');
   ctx = canvas.getContext("2d");
   canvas.width = 600;
@@ -98,54 +118,47 @@ function main(){
   addEventListener('mousemove', mouseMoved, false);
   addEventListener('click', mouseClicked, false);
 
-
-  stacks = {
-    firststack: new Stack(dishhorspacing, ctx),
-    secondstack: new Stack((2*dishhorspacing)+dishwidth, ctx),
-    thirdstack: new Stack((3*dishhorspacing)+(2*dishwidth), ctx),
-    fourthstack: new Stack((4*dishhorspacing)+(3*dishwidth), ctx),
-    fifthstck: new Stack((5*dishhorspacing)+(4*dishwidth), ctx),
-  }
-
-  for (var i = 0; i <5; i++) {
-    stacks.firststack.push(new Dish(i, 'blue', 10, 10));
-  }
-
-  for (var i = 0; i <5; i++) {
-    stacks.secondstack.push(new Dish(i, 'blue', 10, 10));
+  dishwidth = (canvas.width-(numberofstacks*dishhorspacing))/numberofstacks;
+  stacks = {};
+  for (var i = 1; i<=numberofstacks; i++) {
+    console.log((i*dishhorspacing)+((i-1)*dishwidth))
+    stacks["stack"+i] = new Stack((i*dishhorspacing)+((i-1)*dishwidth), ctx) //cleverly assign a name using string addition!
   }
 
 
-  gameLoop();
+  for (var i = 0; i <2; i++) {
+    stacks.stack1.push(new Dish(i, 'blue', 10, 10));    //let's do this with "for...in" in the next iteration
+  }
+
+  for (var i = 0; i <2; i++) {
+    stacks.stack2.push(new Dish(i, 'blue', 10, 10));
+  }
 }
 
+
 function gameLoop(){
-//  processPlayerInput();
+//  processPlayerInput(); //right now we're just doing event handling; later we'll add some plates on a timer
 //  update GameLogic();
   draw();
   setTimeout(gameLoop, 25);
-
 }
-
 
 
 function draw(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // ctx.drawImage(playerImg, mouseX, mouseY);
-
-  for (thisstack in stacks){
+  for (var thisstack in stacks){    //go through and draw all the stacks
       stacks[thisstack].draw();
     }
-  if (carrying){
+
+  if (carrying){        //if the mouse cursor should be a plate
     ctx.fillStyle = carried.color;
     ctx.fillRect(mouseX,mouseY, dishwidth,dishwidth);
   }
 }
 
 
-
-function  mouseMoved(e) {
+function  mouseMoved(e) {     //e is the event handed to us
     mouseX = e.pageX - canvas.offsetLeft;
     mouseY = e.pageY - canvas.offsetTop;
 }
@@ -155,28 +168,16 @@ function  mouseClicked(e) {
     console.log("click");
     mouseX = e.pageX - canvas.offsetLeft;
     mouseY = e.pageY - canvas.offsetTop;
-    for (thisstack in stacks){
-      if(mouseX>stacks[thisstack].x && mouseX<(stacks[thisstack].x+dishwidth)){
-        console.log(carrying);
+    for (var thisstack in stacks){
+      if(mouseX>stacks[thisstack].x && mouseX<(stacks[thisstack].x+dishwidth)){   //if we're in the horizontal zone of a stack
         if(carrying){
           stacks[thisstack].push(carried);
           carrying = false;
         }
         else{
           carried = stacks[thisstack].pop();
-          console.log(carried.color);
           carrying = true;
         }
       }
     }
-
 }
-
-
-    // if (mouseX<100){
-    //   if (stacks.firststack.length()>0){
-    //    stacks.secondstack.push(stacks.firststack.pop());
-    //   }
-    // }
-    // else stacks.firststack.push(new Dish(10, 'blue', 10, 10));
-
